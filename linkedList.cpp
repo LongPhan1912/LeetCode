e4dd59f7c1acea8c51c18f21ba9ab56a77646a18
LongPhan1912 <46760280+LongPhan1912@users.noreply.github.com>
2021-01-24 14:50:06 -0600

    add linked list in binary tree problem

--- linkedList.cpp
+++ linkedList.cpp
@@ -2,7 +2,7 @@
 ListNode* swapNodes(ListNode* head, int k) {
     if (head == NULL || head->next == NULL) { return head; }
     vector<int> array;
-    
+
     ListNode* curr = head;
     while (curr != NULL) {
         array.push_back(curr->val);
@@ -10,7 +10,7 @@ ListNode* swapNodes(ListNode* head, int k) {
     }
     int inverse = array.size() - k;
     swap(array[k-1], array[inverse]);
-    
+
     curr = new ListNode(array[0]);
     ListNode* newHead = curr;
     for (int i = 1; i < array.size(); i++) {
@@ -27,18 +27,18 @@ ListNode* swapNodes(ListNode* head, int k) {
 // memory-efficient method (you can swap the values of the nodes in C++)
 ListNode* swapNodes(ListNode* head, int k) {
     if (head == NULL || head->next == NULL) { return head; }
-    
+
     int size = 0;
     ListNode* curr = head;
     while (curr != NULL) {
         size++;
         curr = curr->next;
     }
-    
+
     int inverse = size - k + 1;
     int i = 1; // the list is 1-indexed
     ListNode* ptr1, *ptr2;
-    
+
     curr = head;
     while (curr) {
         if (i == k) { ptr1 = curr; }
@@ -51,6 +51,38 @@ ListNode* swapNodes(ListNode* head, int k) {
 }
 // Time complexity: O(N)
 // Space complexity: O(1)
+
+// 1367. Linked List in Binary Tree (Medium)
+// Easy explanation:
+bool isSubPath(ListNode* head, TreeNode* root) {
+    return DFS(head, root);
+}
+bool match(ListNode* head, TreeNode* root) {
+    if (head == NULL) { return true; }
+    if (root == NULL || (head->val != root->val)) { return false; }
+    return match(head->next, root->left) || match(head->next, root->right);
+}
+bool DFS(ListNode* head, TreeNode* root) {
+    if (root == NULL) { return false; }
+    if (match(head, root)) { return true; }
+    return DFS(head, root->left) || DFS(head, root->right);
+}
+// More condensed code:
+bool isSubPath(ListNode* head, TreeNode* root) {
+    if (head == NULL) { return true; }
+    if (root == NULL) { return false; }
+    return DFS(head, root)
+        || isSubPath(head, root->left) || isSubPath(head, root->right);
+}
+bool DFS(ListNode* head, TreeNode* root) {
+    if (head == NULL) { return true; }
+    if (root == NULL) { return false; }
+    return head->val == root->val
+        && (DFS(head->next, root->left) || DFS(head->next, root->right));
+}
+
+// Time complexity: O(N * min(treeHeight, listLength))
+// Space complexity: O(treeHeight)
 
 // 1290. Convert Binary Number in a Linked List to Integer (Easy)
 int getDecimalValue(ListNode* head) {
@@ -89,7 +121,7 @@ vector<int> nextLargerNodes(ListNode* head) {
     if (head == NULL) { return ans; }
     while (head) {
         ans.push_back(head->val);
-        head = head->next;   
+        head = head->next;
     }
     stack<int> s;
     for (int i = ans.size() - 1; i > -1; i--) {
@@ -120,7 +152,7 @@ bool isPalindrome(ListNode* head) {
     if (head == NULL || head->next == NULL) { return true; }
     ListNode* curr = head;
     std::stack<int> s;
-    
+
     while (curr) {
         s.push(curr->val);
         curr = curr->next;
@@ -131,16 +163,16 @@ bool isPalindrome(ListNode* head) {
         if (t != head->val) { return false; }
         head = head->next;
     }
-    return true; 
+    return true;
 }
 // Time complexity: O(n)
 // Space complexity: O(n)
 
 // 206. Reverse Linked List (Easy)
-ListNode* reverseList(ListNode* head) {    
+ListNode* reverseList(ListNode* head) {
     ListNode* curr = head;
     ListNode* prev = NULL;
-    
+
     while (curr) {
         ListNode* next = curr->next;
         curr->next = prev;
@@ -156,11 +188,11 @@ ListNode* reverseList(ListNode* head) {
 // 203. Remove Linked List Elements (Easy)
 ListNode* removeElements(ListNode* head, int val) {
     if (head == NULL) { return head; }
-    
+
     // check for all integers with value val at the front
     while (head && head->val == val) { head = head->next; }
     if (head == NULL) { return head; }
-    
+
     ListNode* curr = head;
     ListNode* prev = NULL;
     while (curr) {
@@ -239,7 +271,7 @@ ListNode* reverseBetween(ListNode* head, int m, int n) {
     ListNode* curr = head;
     ListNode *before=NULL, *after=NULL, *prev=NULL;
     ListNode *start, *end;
-    
+
     int i = 1;
     while (curr) {
         // store the original next destination
@@ -270,7 +302,7 @@ ListNode* deleteDuplicates(ListNode* head) {
     if (head == NULL) { return head; }
     ListNode* prev = head;
     prev->next = head->next;
-    
+
     ListNode* curr = head->next;
     while (curr) {
         if (prev->val == curr->val) {
@@ -289,12 +321,12 @@ ListNode* deleteDuplicates(ListNode* head) {
 // 61. Rotate List (Medium)
 ListNode* rotateRight(ListNode* head, int k) {
     if (head == NULL || k == 0) { return head; }
-    
+
     ListNode* left = NULL;
     ListNode* right = head;
-    
+
     ListNode* curr = head;
-    int size = 0; 
+    int size = 0;
     // figure out the size of the linked list
     while (curr != NULL) {
         size += 1;
@@ -302,25 +334,25 @@ ListNode* rotateRight(ListNode* head, int k) {
     }
     k = k % size; // in case k is greater than size, take the remainder
     if (k == 0 || size == 1) { return head; }
-    
+
     curr = head;
     int i = 0;
     // figure out where the end of our left and right sublist are
     ListNode* endOfRight = NULL;
     ListNode* endOfLeft = NULL;
     int splitPoint = size - k;
-    
+
     while (curr != NULL) {
         if (i < splitPoint) { endOfRight = curr; }
-        else if (i == splitPoint) { left = curr; } 
+        else if (i == splitPoint) { left = curr; }
         else { endOfLeft = curr; }
         curr = curr->next;
         i += 1;
     }
-    
+
     endOfRight->next = NULL;
     // check whether the end of our left sublist is null or not
-    if (endOfLeft) { endOfLeft->next = head; } 
+    if (endOfLeft) { endOfLeft->next = head; }
     else { left->next = head; }
     // reset the head
     head = left;
@@ -338,7 +370,7 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         // time for one list to join the other
         if (l1 == NULL) { curr->next = l2; break; }
         if (l2 == NULL) { curr->next = l1; break; }
-        // if there's value in the first list that is less than or equal 
+        // if there's value in the first list that is less than or equal
         // to that in the second list, then tie curr's next to the node in the first list
         if (l1->val <= l2->val) {
             curr->next = l1;
