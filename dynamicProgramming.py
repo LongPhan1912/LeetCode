f4b317b02e12ee4939b776fda30e5620b9aca973
LongPhan1912 <46760280+LongPhan1912@users.noreply.github.com>
2021-01-26 23:57:33 -0600

    ways to make fair array (medium)

--- dynamicProgramming.py
+++ dynamicProgramming.py
@@ -1,13 +1,31 @@
+# 1664. Ways to Make a Fair Array (Medium)
+def waysToMakeFair(self, nums: List[int]) -> int:
+    count = 0
+    right = [0,0]
+    left = [0, 0]
+
+    for idx, num in enumerate(nums):
+        right[idx % 2] += num
+
+    for idx, num in enumerate(nums):
+        right[idx % 2] -= num
+        if (left[0] + right[1] == left[1] + right[0]):
+            count += 1
+        left[idx % 2] += num
+    return count
+# Time complexity: O(N)
+# Space complexity: O(1)
+
 # 1314. Matrix Block Sum (Medium)
 def matrixBlockSum(self, mat: List[List[int]], K: int) -> List[List[int]]:
     m = len(mat)
     n = len(mat[0])
     prefix = [[0] * (n+1) for _ in range(m+1)]
-    
+
     for i in range(m):
         for j in range(n):
             prefix[i+1][j+1] = mat[i][j] + prefix[i+1][j] + prefix[i][j+1] - prefix[i][j]
-            
+
     # Explanation for prefix sum: https://www.youtube.com/watch?v=PwDqpOMwg6U
 
     res = [[0] * (n) for _ in range(m)]
@@ -20,7 +38,7 @@ def matrixBlockSum(self, mat: List[List[int]], K: int) -> List[List[int]]:
             res[i][j] = prefix[down][right] - prefix[up][right] - \
                         prefix[down][left] + prefix[up][left]
 
-    # Explanation for res table: https://leetcode.com/problems/matrix-block-sum/discuss/477036/JavaPython-3-PrefixRange-sum-w-analysis-similar-to-LC-30478             
+    # Explanation for res table: https://leetcode.com/problems/matrix-block-sum/discuss/477036/JavaPython-3-PrefixRange-sum-w-analysis-similar-to-LC-30478
     return res
 
 # Time complexity: O(m * n)
@@ -34,11 +52,11 @@ def divisorGame(self, N: int) -> bool:
 # Space complexity: O(1)
 
 # 746. Min Cost Climbing Stairs (Easy)
-def minCostClimbingStairs(self, cost: List[int]) -> int:      
+def minCostClimbingStairs(self, cost: List[int]) -> int:
     n = len(cost)
     for i in range(2, n):
         cost[i] += min(cost[i-1], cost[i-2])
-        
+
     return min(cost[n - 1], cost[n - 2])
 
 # Time complexity: O(n)
@@ -49,12 +67,12 @@ def minCostClimbingStairs(self, cost: List[int]) -> int:
 def isSubsequence(self, s: str, t: str) -> bool:
     pt1 = 0
     pt2 = 0
-    
+
     while (pt1 < len(s) and pt2 < len(t)):
         if (s[pt1] == t[pt2]):
             pt1 += 1
         pt2 += 1
-    
+
     return pt1 == len(s)
 
 # Time complexity: O(n)
@@ -65,11 +83,11 @@ def maxProfit(self, prices: List[int]) -> int:
     mini = float('inf')
     profit = 0
     for price in prices:
-        if mini > price: 
+        if mini > price:
             mini = price
         elif profit < price - mini:
             profit = price - mini
-    
+
     return profit
 # Time complexity: O(n)
 # Space complexity: 0(1)
@@ -81,7 +99,7 @@ def climbStairs(self, n: int) -> int:
     dp[1] = 2
     for i in range(2, n):
         dp[i] = dp[i-1] + dp[i-2]
-    
+
     return dp[n-1]
 
 # Time complexity: O(n)
@@ -101,7 +119,7 @@ def maxSubArray(self, nums: List[int]) -> int:
     n = len(nums)
     if n == 1:
         return nums[0]
-    
+
     max_sum = float('-inf')
     curr_sum = 0
     for num in nums:
@@ -113,4 +131,3 @@ def maxSubArray(self, nums: List[int]) -> int:
 
 # Time complexity: O(n)
 # Space complexity: O(1)
-
