c47a73def17e8fd8e50c2bb0021953b0dae4349e
LongPhan1912 <46760280+LongPhan1912@users.noreply.github.com>
2021-02-12 23:51:45 -0600

    Deepest Leaves Sum (Medium)

--- trees.cpp
+++ trees.cpp
@@ -1,3 +1,30 @@
+// 1302. Deepest Leaves Sum (Medium)
+class Solution {
+private:
+    int sum = 0;
+    int maxLevel = 0;
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        if (!root) return 0;
+        sumHelper(root, 0);
+        return sum;
+    }
+    void sumHelper(TreeNode* root, int level) {
+        if (!root) return;
+        if (level > maxLevel) {
+            maxLevel = level;
+            sum = 0;
+        } 
+        if (level == maxLevel) {
+            sum += root->val;
+        }
+        sumHelper(root->left, level+1);
+        sumHelper(root->right, level+1);
+    }
+};
+// Time complexity: O(N)
+// Space complexity: O(1)
+
 // 108. Convert Sorted Array to Binary Search Tree (Easy)
 TreeNode* sortedArrayToBST(vector<int>& nums) {
     if (nums.size() == 0) {
